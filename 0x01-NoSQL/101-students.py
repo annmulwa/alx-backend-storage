2a78c91f65000b0b3b66f6a923407af2141ef48a
annmulwa <annmulwa2000@gmail.com>
2024-07-16 11:08:38 +0000

    NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Python function that returns all students sorted by average score
+'''
+
+
+def top_students(mongo_collection):
+    '''
+    average score must be part of each item returns with key = averageScore
+    '''
+     top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
