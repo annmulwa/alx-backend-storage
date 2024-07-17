b2a73f3c9a165681733436e4406a9757ae211b62
annmulwa <annmulwa2000@gmail.com>
2024-07-17 13:08:48 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,3 +25,31 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[str, bytes, int, float]:
+        '''
+        convert the data back to the desired format.
+        '''
+        data = self._redis.get(key)
+        if fn:
+            return fn(callable)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''
+        convert the data back to str format
+        '''
+        variable = self._redis.get(key)
+        return variable.decode("UTF-8")
+
+    def get_int(self, key: str) -> int:
+        '''
+        convert the data back to int format
+        '''
+        variable = self._redis.get(key)
+        try:
+            variable = int(variable.decode("UTF-8"))
+        except Exception:
+            variable = 0
+        return variable
