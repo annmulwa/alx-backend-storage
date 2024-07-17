f94d0003673fdebb4710776616b0e01d1636cfe5
annmulwa <annmulwa2000@gmail.com>
2024-07-17 14:18:28 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,6 +28,29 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    '''
+    has a single parameter named method
+    that is a Callable and returns a Callable
+    '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''
+        add its input parameters to one list in
+        redis, and store its output into another list
+        '''
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(input_key, str(args))
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
+
 class Cache():
     '''
     A cache class
@@ -37,6 +60,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         method should generate a random key
